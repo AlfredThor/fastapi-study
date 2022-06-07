5cca1dd4b29f7f4f23798983a8cd8cace04de311
hella <hellathorchmod777@gmail.com>
2022-06-07 16:11:03 +0800

    'firsts'

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    result = {'code':200,'message':'请求成功！'}
+    return result
 
 if __name__ == '__main__':
     uvicorn.run(app=app, host='127.0.0.1', port=8000)
